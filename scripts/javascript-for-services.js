21df6ee1d50d0ada171f4f78830fedc4af8fc62b
rodionion <rodionvh@gmail.com>
2022-10-01 13:31:22 +0200

    Add Services and Appointment pages

---
+++ scripts/javascript-for-services.js
@@ -0,0 +1,9 @@
+  for (let i = 1; i < 9; i++) {
+    const animation = bodymovin.loadAnimation({
+        container: document.getElementById(`animation-container-${i}`),
+        path: `graphics/animations/${i}.json`,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+      });
+  }
