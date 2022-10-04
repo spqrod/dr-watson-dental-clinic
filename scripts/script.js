83bc55c324a6fd00daf06287a90864ecd1c76e2c
rodionion <rodionvh@gmail.com>
2022-10-04 07:38:44 +0200

    Polish responsiveness

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+const copyrightText = document.getElementById("copyright-text");
+const currentYear = new Date().getFullYear();
+copyrightText.innerText = `Copyright © ${currentYear} Dr Watson Dental Clinic`;
