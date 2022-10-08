26985c789d2947dabe265e66e369aa54f621442d
Rodion <rodionvh@gmail.com>
2022-10-08 10:15:06 +0200

    Fix minor bugs

--- scripts/script.js
+++ scripts/script.js
@@ -35,7 +35,7 @@ class header extends HTMLElement {
 
         <!-- Header Logo -->
 
-        <a class="logo" href="https://drwatsondental.com/">
+        <a class="logo" href="https://drwatsondental.com">
             <img src="${urlPrefix}graphics/logo.jpg" alt="dr-watson-dental-clinic-logo"></a>
 
         <!-- Header hamburger icon -->
@@ -53,10 +53,10 @@ class header extends HTMLElement {
 
             <ul>
 
-                <li><a class="menulinks" href="https://drwatsondental.com/about/">About</a></li>			
-                <li><a class="menulinks" href="https://drwatsondental.com/contact/">Contact</a></li>
-                <li><a class="menulinks" href="https://drwatsondental.com/appointment/">Appointment</a></li>
-                <li><a class="menulinks" href="https://drwatsondental.com/services/">Services</a></li>
+                <li><a class="menulinks" href="https://drwatsondental.com/about">About</a></li>			
+                <li><a class="menulinks" href="https://drwatsondental.com/contact">Contact</a></li>
+                <li><a class="menulinks" href="https://drwatsondental.com/appointment">Appointment</a></li>
+                <li><a class="menulinks" href="https://drwatsondental.com/services">Services</a></li>
                 <li class="phone-and-whatsapp-in-nav"><a href="tel:+260955336825">Call +260 955 336825</a></li>
                 <li class="phone-and-whatsapp-in-nav"><a class="social" href="https://wa.me/260955336825">
                     <img class="social-image" src="${urlPrefix}graphics/whatsapp.png"></a></li>
