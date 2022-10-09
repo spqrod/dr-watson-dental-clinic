448ea941a97c4a052491e96a67ea605d8ba381d3
Rodion <rodionvh@gmail.com>
2022-10-09 14:34:09 +0200

    Update email-form.php

--- email-form.php
+++ email-form.php
@@ -1,39 +1,53 @@
 <?php
 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require '/PHPMailer-master/src/Exception.php';
-require '/PHPMailer-master/src/PHPMailer.php';
-require '/PHPMailer-master/src/SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+require 'smtp-account-details.php';
 
-$to = "rodionvh@gmail.com";
-$nameto = "nameto";
-$subject = "subject";
-$message = "message";
+$mail = new PHPMailer(true);
 
-function sendmail($to,$nameto,$subject,$message,$altmess)  {
-    $from  = "care@drwatsondetal.com"; 
-    $namefrom = "Your Name";
-    $mail = new PHPMailer();
-    $mail->SMTPDebug = 0;
-    $mail->CharSet = 'UTF-8';
-    $mail->isSMTP();
-    $mail->SMTPAuth   = true;
-    $mail->Host   = "mail.privateemail.com";
-    $mail->Port       = 465;
-    $mail->Username   = $from;
-    $mail->Password   = "Radha108!";
-    $mail->SMTPSecure = "ssl";
-    $mail->setFrom($from,$namefrom);
-    $mail->addCC($from,$namefrom);
-    $mail->Subject  = $subject;
-    $mail->isHTML();
-    $mail->Body = $message;
-    $mail->AltBody  = $altmess;
-    $mail->addAddress($to, $nameto);
-    return $mail->send();
-  }
+$mail->SMTPDebug = 3;                               
+
+$mail->isSMTP();            
+                     
+$mail->Host = "mail.privateemail.com";
+
+$mail->SMTPAuth = true;                          
+
+$mail->Username = $smtpUsername;     
+
+$mail->Password = $smtpPassword;                           
+
+$mail->SMTPSecure = "ssl";                           
+
+$mail->Port = 465;                                   
+
+$mail->From = "care@drwatsondental.com";
+
+$mail->FromName = "Email from our website";
+
+$mail->addAddress("rodionvh@gmail.com", "Recepient Name");
+
+$mail->isHTML(true);
+
+$mail->Subject = "This email was sent from our website using the contact form";
+$mail->Body = "<p>Do not reply to this email as it will not be delivered. Instead, contact the patient using the details they provided below (email or phone).<br><br>Name: $name.<br><br>Email: $email.<br><br>Phone: $phone.<br><br>Message: $message.</p>";
+
+try {
+    $mail->send();
+    echo "Message has been sent successfully";
+} catch (Exception $e) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
 
 ?>
